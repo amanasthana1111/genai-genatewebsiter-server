1b7da86ffb229145296ce81f94f57934566cbb6c
Aman Asthana <120777911+amanasthana1111@users.noreply.github.com>
2025-08-01 10:54:15 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,55 @@
+import { GoogleGenAI } from "@google/genai";
+import cors from "cors";
+
+import dotenv from "dotenv";
+dotenv.config();
+
+import express from "express";
+
+const app = express();
+app.use(cors())
+app.use(express.json())
+
+const ai = new GoogleGenAI({});
+app.get("/", (req ,res)=>{res.json({"message ": "running"})})
+app.post("/", async (req, res) => {
+  const requestfromclient = req.body.prompt;
+  console.log("enter " + requestfromclient);
+  try {
+    const response = await ai.models.generateContent({
+      model: "gemini-2.5-flash",
+      contents: `${requestfromclient}`,
+      config: {
+        systemInstruction:
+          "You are a website code generator. You only respond with complete, functional website code in JSON format containing HTML, CSS, and JavaScript. The websites you generate must be responsive, use modern CSS features,with cool animations and hover effect , have attractive styling and color schemes, and include all necessary functionality. Never include any explanatory text or sentences - only pure code in the specified format.",
+      },
+    });
+
+    let raw = response.text;
+    raw = raw
+      .replace(/^```json/, "")
+      .replace(/```$/, "")
+      .trim();
+      
+    const webData = JSON.parse(raw);
+    // console.log(webData)
+    const htmlData = webData.html;
+    const cssData = webData.css;
+    const jsDATA = webData.javascript;
+
+    res.json({
+      html: htmlData,
+      css: cssData,
+      js: jsDATA,
+    });
+  } catch (error) {
+    res
+      .json({
+        message: "Something Wrong",
+        error: error,
+      })
+      .status(500);
+  }
+});
+
+app.listen(3000);
